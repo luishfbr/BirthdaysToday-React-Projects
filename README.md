a19719db3a42c6e7c312ce9c9c61e98ecef69dd8
luishfbr <98354869+luishfbr@users.noreply.github.com>
2022-11-22 11:40:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BirthdaysToday-React-Projects
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-uz14gx)
---
+++ data.tsx
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Matheus Alberto',
+    age: 23,
+    image: 'https://randomuser.me/api/portraits/men/59.jpg',
+  },
+  {
+    id: 2,
+    name: 'Anderson Filfode',
+    age: 45,
+    image: 'https://randomuser.me/api/portraits/men/34.jpg',
+  },
+  {
+    id: 3,
+    name: 'Yuri Thaison',
+    age: 26,
+    image: 'https://randomuser.me/api/portraits/men/79.jpg',
+  },
+  {
+    id: 4,
+    name: 'Glauco Malaquias',
+    age: 54,
+    image: 'https://randomuser.me/api/portraits/men/23.jpg',
+  },
+  {
+    id: 5,
+    name: 'Mathias Frederico',
+    age: 38,
+    image: 'https://randomuser.me/api/portraits/men/50.jpg',
+  },
+];
